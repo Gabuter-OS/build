1ecec87ed873af987693362497e7d6ead787b1d7
Heinz Der Flugel <heinzdf@gabuters-dev.my.id>
2021-06-03 13:05:50 +0700

    build: Don't include plus version

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -819,14 +819,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
 
-  is_plus = target_info.GetBuildProp("org.gabuters.version").endswith(
-        "_plus")
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
   build_date = target_info.GetBuildProp("org.gabuters.build_date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
   device = target_info.GetBuildProp("org.gabuters.device")
-  script.PrintGabutersBanner(is_plus, android_version, build_id, build_date,
+  script.PrintGabutersBanner(android_version, build_id, build_date,
                                   security_patch, device)
 
   device_specific.FullOTA_InstallBegin()
